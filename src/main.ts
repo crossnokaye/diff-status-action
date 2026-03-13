fdd83933188641e9d6c39f13be6ed6afd435422f
Neha Viswanathan <12013126+neha-viswanathan@users.noreply.github.com>
2026-03-13 10:22:35 -0700

    Add docs-only output for conditional job skipping (#10)
    * Add docs-only output for conditional job skipping
    
    * refactor: generic output name (not docs specific)
    
    ---------
    
    Co-authored-by: Tony Spataro <tony@crossnokaye.com>

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,8 @@ export async function run(): Promise<void> {
       globs.some((glob: string) => minimatch(file.filename, glob))
     );
 
+    core.setOutput('all-match', allFilesMatch.toString());
+
     if (!allFilesMatch) {
       core.info('Changed files are not exempt from required statuses');
       return;
